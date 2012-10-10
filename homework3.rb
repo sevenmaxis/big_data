cc413317a9ac61a849d32bf8eedec21d0a589024
sevenmaxis <Sergey.Gopkalo@gmail.com>
2012-10-10 19:30:39 +0300

    added more functional look

--- homework3.rb
+++ homework3.rb
@@ -18,43 +18,39 @@ Mandy.job "Author-term maping" do
   reduce_tasks 3
   
   map do |value|
-    # { author1 => { term1 => count, term2 => count }, author2 => { ... }, ... }
-    hash = Hash.new{ |h,k| h[k] = Hash.new(0) }
     
     update_status("Processing line: #{value}")
 
     authors = value.scan(/(?<=::)[^:].*?(?=::)/)
-    terms = value.match(/.*:::(.*)/)[1].downcase.gsub(/\W|[0-9]|\ba\b|-/, ' ').split
-
-    # delete stop words from list of terms
+    # find terms in a string
+    terms = value.scan(/(?!.*:).+/).downcase.gsub(/\W|[0-9]|\ba\b|-/, ' ').split
+    # delete stop words
     terms -= $stop_words
 
-    authors.each do |author| 
-      terms.each do |term|       
-        #increment_counter("Author #{author} processed",  "The term #{term} processed")
-        hash[author][term] += 1
-      end
-    end
+    # hash = { author1 => { term1 => count, term2 => count }, author2 => {...}, ...}
+    # h_* means that this variable has hash structure
+    authors.each_with_object(Hash.new{|h,k|h[k]=Hash.new(0)}) do |author, hash| 
 
-    # h_ means this variable has hash structure
-    hash.each do |author, h_term|
-      h_term.each do |term, count|
-        emit( author, "#{term}:#{count}")
-      end
+      terms.each { |term| hash[author][term] += 1 }
+
+    end.each do |author, h_terms|
+
+      h_terms.each { |term, count| emit( author, "#{term}:#{count}") }
+
     end
   end
   
   reduce do |author, h_terms|
 
-    hash = Hash.new(0)
+    h_terms.each_with_object(Hash.new(0)) do |h_term, hash|
 
-    h_terms.each do |h_term|
       term, count = h_term.split(':')
       hash[term] += count.to_i
-    end
 
-    hash.sort_by{ |_,count| -count }.each do |term, count|
+    end.sort_by{ |_,count| -count }.each do |term, count|
+
       emit( author, "#{term}:#{count}")
+
     end
   end
 end
